12b0e15bc9389b75d63d7d17727a755842f881ff
Renan_Fonseca <renanfonseca88@gmail.com>
2023-01-09 23:43:22 -0300

    Adicionando combo Box ao formulario.

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React from 'react';
 import './Formulario.css';
 import CampoTexto from '../CampoTexto/CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 
 const Formulario = () => {
-  return(
-    <section className="formulario">
-      <form action="">
-        <h2>Preencha os dados para criar os cards dos colaboradores</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
-        <CampoTexto label="Imagem"placeholder="Digite o endereço da sua imagem"/>
-      </form>
-    </section>
-  )
-}
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão',
+    ];
+    return (
+        <section className="formulario">
+            <form action="">
+                <h2>Preencha os dados para criar os cards dos colaboradores</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da sua imagem"
+                />
+                <ListaSuspensa label="Time" itens={times} />
+            </form>
+        </section>
+    );
+};
 
 export default Formulario;
