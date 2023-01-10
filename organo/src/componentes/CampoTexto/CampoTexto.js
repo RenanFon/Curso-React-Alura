f2c6766374488632caaacaf1e4d0fcde22d95764
Renan_Fonseca <renanfonseca88@gmail.com>
2023-01-10 18:42:32 -0300

    Adicionando Permissoes

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
-  return (
-    <div className="campo-texto">
-      <label>{props.label}</label>
-      <input placeholder={props.placeholder} type="text" />
-    </div>
-  );
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input
+                required={props.obrigatorio}
+                placeholder={props.placeholder}
+                type="text"
+            />
+        </div>
+    );
 };
 
 export default CampoTexto;
