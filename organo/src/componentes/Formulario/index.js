d5e4db08f2206fa7f1a9fc36c2b429efe4415100
Renan_Fonseca <renanfonseca88@gmail.com>
2023-01-15 21:03:28 -0300

    Adicionando Times.

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -5,15 +5,7 @@ import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão',
-    ];
+  
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -57,7 +49,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label="Time" 
-                    itens={times} 
+                    itens={props.times} 
                     valor={time}
                     AoAlterado={valor => setTime(valor)}
                 />
